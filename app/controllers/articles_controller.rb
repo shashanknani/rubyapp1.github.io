98a24db75a9c06bbd93ac528bad2ba7601f3b7eb
shashanknani <shashank-n@outlook.com>
2018-12-05 21:41:45 -0500

    added user association between user and article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,17 +8,17 @@ class ArticlesController < ApplicationController
     end
     
     def new
-        @article = Article.new
+        @article = current_user.articles.build
     end
     
     def create
-        @article = Article.new(article_params)
+        @article = current_user.articles.build(article_params)
         if @article.save
             redirect_to @article 
         else
             render 'new'
         end
-    end
+@art    end
 
    private
 
